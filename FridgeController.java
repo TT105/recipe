fad6c17dd27a954ddaf0544748ab0833f7412ef3
TT105 <tougo040213@gmail.com>
2025-05-16 19:13:04 +0900

    Create FridgeController.java

---
+++ FridgeController.java
@@ -0,0 +1,22 @@
+@RestController
+public class FridgeController {
+
+    @Autowired
+    private YoloService yoloService;
+    @Autowired
+    private ChatGptService chatGptService;
+
+    @PostMapping("/upload")
+    public String handleImageUpload(@RequestParam("file") MultipartFile file, Model model) throws IOException {
+        List<String> ingredients = yoloService.detectIngredients(file);
+        String recipe = chatGptService.generateRecipe(ingredients);
+        model.addAttribute("ingredients", ingredients);
+        model.addAttribute("recipe", recipe);
+        return "index";
+    }
+
+    @GetMapping("/")
+    public String index() {
+        return "index";
+    }
+}
